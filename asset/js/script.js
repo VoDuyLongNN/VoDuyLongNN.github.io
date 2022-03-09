b54036eca56cda62b25290c718befffce4b133f2
Võ Duy Long <89375882+VoDuyLongNN@users.noreply.github.com>
2022-03-09 22:58:15 +0700

    upload

---
+++ asset/js/script.js
@@ -0,0 +1,49 @@
+// javaScript login form
+const loginDiv = document.querySelector('.js-login');
+const model = document.querySelector('.js-model');
+const closeFormLogin = document.querySelector('.js-close-form');
+
+function showFormLogin(){
+   model.classList.add('open');
+}
+
+function hideFormLogin(){
+   model.classList.remove('open');
+}
+
+loginDiv.addEventListener('click', showFormLogin);
+closeFormLogin.addEventListener('click', hideFormLogin);
+
+// javaScript search form
+const searchIcon = document.querySelector('.js-right-header-search');
+const searchForm = document.querySelector('.js-search');
+const closeSearchForm = document.querySelector('.js-close-search');
+
+function showFormSearch(){
+   searchForm.classList.add('open');
+}
+
+function hideFormSearch(){
+   searchForm.classList.remove('open');
+}
+
+searchIcon.addEventListener('click', showFormSearch);
+closeSearchForm.addEventListener('click', hideFormSearch);
+
+
+// javaScript card shopping form
+const cardIcon = document.querySelector('.js-right-header-card');
+const shoppingForm = document.querySelector('.js-shopping-form');
+const closeShoppingForm = document.querySelector('.js-close-shopping-form');
+
+function showFormShopping(){
+   shoppingForm.classList.add('open');
+}
+
+function hideFormShopping(){
+   shoppingForm.classList.remove('open');
+}
+
+cardIcon.addEventListener('click', showFormShopping);
+closeShoppingForm.addEventListener('click', hideFormShopping)
+
---
+++ asset/js/slickSlider.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+   $(".slick-slide").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    draggable: false,
+    dots: true,
+    prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+   });
+ });
