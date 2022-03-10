27d77d414f0afc0f96b631c5940c04142e122833
Võ Duy Long <89375882+VoDuyLongNN@users.noreply.github.com>
2022-03-10 14:34:47 +0700

    upload

--- asset/js/slickSlider.js
+++ asset/js/slickSlider.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
     draggable: false,
     dots: true,
     prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-    nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
-    // autoplay: true,
-    // autoplaySpeed: 1000,
+    nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    autoplay: true,
+    autoplaySpeed: 1000,
    });
  });
